cf558ed62e4b6c3b577ba40644622406db46cedb
Maria Rojas <alu0100605983@ull.edu.es>
2014-10-23 23:58:39 +0200

    Añadiendo gemas y completando get'/'

--- app.rb
+++ app.rb
@@ -40,7 +40,7 @@ Base = 36
 session[:email] = " "
 
 get '/' do
-
+  puts "inside get '/': #{[params]}"
   @list = ShortenedUrl.all(:order => [ :id.asc ], :limit => 20)
   # in SQL => SELECT * FROM "ShortenedUrl" ORDER BY "id" ASC
   haml :index
